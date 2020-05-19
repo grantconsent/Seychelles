d422b4304b1358e1b6b3d60fe9bbce13daf71cf8
MajorE <emmanuel.1aliyu@gmail.com>
2020-05-19 23:52:05 +0100

    Added Toast notification Asks user to add picture if image is null and Continue is clicked

--- lib/screens/get_user_picture.dart
+++ lib/screens/get_user_picture.dart
@@ -5,6 +5,10 @@ import 'package:flutter/material.dart';
 import 'package:grantconsent/utilities/constants.dart';
 import 'package:grantconsent/utilities/custom_widgets.dart';
 
+import '../utilities/styles.dart';
+import '../utilities/styles.dart';
+import '../utilities/styles.dart';
+
 class GetUserPicture extends StatefulWidget {
   @override
   _GetUserPictureState createState() => _GetUserPictureState();
@@ -13,8 +17,9 @@ class GetUserPicture extends StatefulWidget {
 class _GetUserPictureState extends State<GetUserPicture> {
   File _uploadedImage;
   bool pictureAdded = false;
+  final keyToScaffold = new GlobalKey<ScaffoldState>();
 
-  Future getImage() async {
+  Future _getImageFromCamera() async {
     var image = await ImagePicker.pickImage(source: ImageSource.camera);
     setState(() {
       _uploadedImage = image;
@@ -25,10 +30,37 @@ class _GetUserPictureState extends State<GetUserPicture> {
       pictureAdded = false;
   }
 
+  void _showToastInformation() {
+    keyToScaffold.currentState.showSnackBar(
+      SnackBar(
+        behavior: SnackBarBehavior.fixed,
+        duration: Duration(seconds: 2),
+        content: Container(
+          alignment: Alignment.center,
+          height: 20,
+          child: Text(
+            'Please, add a picture first.',
+            style: kBody1TextStyle,
+          ),
+        ),
+      ),
+    );
+  }
+
+  ConstrainedBox _buildPlaceholder() {
+    return ConstrainedBox(
+        constraints: BoxConstraints.tightFor(
+          width: kScreenSize.width,
+          height: kScreenSize.height * 0.65,
+        ),
+        child: kUserPicturePlaceholder);
+  }
+
   @override
   Widget build(BuildContext context) {
     kScreenSize = MediaQuery.of(context).size;
     return Scaffold(
+      key: keyToScaffold,
       body: Padding(
         padding: EdgeInsets.symmetric(horizontal: 37),
         child: Column(
@@ -39,14 +71,9 @@ class _GetUserPictureState extends State<GetUserPicture> {
               flex: 3,
             ),
             GestureDetector(
-              onTap: getImage,
+              onTap: _getImageFromCamera,
               child: _uploadedImage == null
-                  ? ConstrainedBox(
-                      constraints: BoxConstraints.tightFor(
-                        width: kScreenSize.width,
-                        height: kScreenSize.height * 0.65,
-                      ),
-                      child: kUserPicturePlaceholder)
+                  ? _buildPlaceholder()
                   : Image.file(
                       _uploadedImage,
                       alignment: Alignment.center,
@@ -59,15 +86,17 @@ class _GetUserPictureState extends State<GetUserPicture> {
               flex: 2,
             ),
             UserActionButton(
-                locked: !pictureAdded,
-                onTap: () {
-                  Navigator.push(
-                    context,
-                    MaterialPageRoute(
-                      builder: (context) => UploadPicture(),
-                    ),
-                  );
-                },
+                //locked: !pictureAdded,
+                onTap: !pictureAdded
+                    ? _showToastInformation
+                    : () {
+                        Navigator.push(
+                          context,
+                          MaterialPageRoute(
+                            builder: (context) => UploadPicture(),
+                          ),
+                        );
+                      },
                 label: 'Continue'),
             Spacer(flex: 3),
           ],
