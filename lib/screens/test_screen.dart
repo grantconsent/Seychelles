5cc03de25bcd297873d14c92e8bd599253bf48cc
Emmanuel Aliyu <53065184+Meghatronics@users.noreply.github.com>
2020-05-23 12:36:43 +0100

    Tested Loader in test screen. Shows while email sends. Changed minSDKversion to 23, to correct cloud firestore error.

--- lib/screens/test_screen.dart
+++ lib/screens/test_screen.dart
@@ -3,6 +3,8 @@ import 'package:grantconsent/services/send_through_email.dart';
 import 'package:grantconsent/services/send_through_whatsapp.dart';
 import 'package:grantconsent/utilities/styles.dart';
 
+import 'loader.dart';
+
 //Use this screen to test your features and stuff.
 
 class TestInterface extends StatefulWidget {
@@ -15,67 +17,77 @@ class _TestInterfaceState extends State<TestInterface> {
   TextEditingController emailController = TextEditingController();
   @override
   Widget build(BuildContext context) {
-    return Scaffold(
-        body: Column(
-      mainAxisAlignment: MainAxisAlignment.center,
-      crossAxisAlignment: CrossAxisAlignment.stretch,
-      children: <Widget>[
-        Text(
-          data1 ?? 'null',
-          style: TextStyle(color: Colors.white, fontSize: 25),
-        ),
-        Text(
-          data2 ?? 'null',
-          style: TextStyle(color: Colors.white, fontSize: 25),
-        ),
-        Text(
-          data3 ?? 'null',
-          style: TextStyle(color: Colors.white, fontSize: 25),
-        ),
-        RaisedButton(onPressed: () async {
-          LocalContact selectedContact = await pickAContact();
-          if (selectedContact != null)
-            sendConsentViaWhatsapp(
-                phoneNumber: selectedContact.phoneNumber[0],
-                link: 'you link input to the function');
-          else
-            return;
+    ValueNotifier _isLoading = ValueNotifier(false);
+    return ValueListenableBuilder(
+      valueListenable: _isLoading,
+      builder: (context, loading, child) {
+        return loading
+            ? Loader()
+            : Scaffold(
+                body: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  crossAxisAlignment: CrossAxisAlignment.stretch,
+                  children: <Widget>[
+                    Text(
+                      data1 ?? 'null',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                    Text(
+                      data2 ?? 'null',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                    Text(
+                      data3 ?? 'null',
+                      style: TextStyle(color: Colors.white, fontSize: 25),
+                    ),
+                    RaisedButton(onPressed: () async {
+                      LocalContact selectedContact = await pickAContact();
+                      if (selectedContact != null)
+                        sendConsentViaWhatsapp(
+                            phoneNumber: selectedContact.phoneNumber[0],
+                            link: 'you link input to the function');
+                      else
+                        return;
 
-          setState(() {
-            data1 = selectedContact.name;
-            //  data2 = selectedContact.emailAddress;
-            data2 = selectedContact.phoneNumber.toString();
-          });
-        }),
-        SizedBox(height: 20),
-        TextField(
-        textInputAction: TextInputAction.send,
-          keyboardType: TextInputType.emailAddress,
-          style: kBody1TextStyle,
-        
-          decoration: InputDecoration(
-
-            helperText: 'Enter your email address',
-            hintText: 'Email Address',
-            hintStyle: kBody1TextStyle,
-            helperStyle: kBody1TextStyle.copyWith(fontSize: 11),
-          ),
-          controller: emailController,
-        ),
-        RaisedButton(onPressed: () async {
-          setState(() {
-            data3 = "- - - -";
-          });
-          MailStatus status =
-              await sendConsentViaEmail(emailController.text, '1234567');
-          setState(() {
-            if (status == MailStatus.success)
-              data3 = 'Sent';
-            else
-              data3 = 'Failed to send';
-          });
-        })
-      ],
-    ));
+                      setState(() {
+                        data1 = selectedContact.name;
+                        //  data2 = selectedContact.emailAddress;
+                        data2 = selectedContact.phoneNumber.toString();
+                      });
+                    }),
+                    SizedBox(height: 20),
+                    TextField(
+                      textInputAction: TextInputAction.send,
+                      keyboardType: TextInputType.emailAddress,
+                      style: kBody1TextStyle,
+                      decoration: InputDecoration(
+                        helperText: 'Enter your email address',
+                        hintText: 'Email Address',
+                        hintStyle: kBody1TextStyle,
+                        helperStyle: kBody1TextStyle.copyWith(fontSize: 11),
+                      ),
+                      controller: emailController,
+                    ),
+                    RaisedButton(
+                      onPressed: () async {
+                        _isLoading.value = true;
+                        MailStatus status = await sendConsentViaEmail(
+                            emailController.text, '1234567');
+                        setState(
+                          () {
+                            if (status == MailStatus.success)
+                              data3 = 'Sent';
+                            else
+                              data3 = 'Failed to send';
+                          },
+                        );
+                        _isLoading.value = false;
+                      },
+                    )
+                  ],
+                ),
+              );
+      },
+    );
   }
 }
