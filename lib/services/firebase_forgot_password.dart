fc280ce9bdd2e899f65e3ee19de4d4f310079219
geektutor <sodiq.akinjobi@gmail.com>
2020-05-31 22:36:28 +0100

    Updated forgot Password

--- lib/services/firebase_forgot_password.dart
+++ lib/services/firebase_forgot_password.dart
@@ -5,18 +5,20 @@ Future forgotPassword(String email) async {
   //TODO: Try and catch exceptions. Return status based on exceptions or success.
   try {
     await _auth.sendPasswordResetEmail(email: email);
-    print('working');
     return ForgotStatus.success;
   } catch (e) {
     print(e);
-    return e.toString().contains('ERROR_USER_NOT_FOUND')
-        ? ForgotStatus.invalid
-        : ForgotStatus.internal;
+    return e.toString().contains('ERROR_INVALID_EMAIL')
+        ? ForgotStatus.incorrect
+        : e.toString().contains('ERROR_USER_NOT_FOUND')
+            ? ForgotStatus.invalid
+            : ForgotStatus.internal;
   }
 }
 
 enum ForgotStatus {
   success,
+  incorrect,
   invalid,
   internal,
 }
