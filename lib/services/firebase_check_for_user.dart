ce29f4deacd1774774fd23a286aed60e2dfd8cd2
Arafah Ogunfemi <ayobamiarafat09@gmail.com>
2020-06-01 20:26:33 +0100

    fix crash and capitalize first letter

--- lib/services/firebase_check_for_user.dart
+++ lib/services/firebase_check_for_user.dart
@@ -5,22 +5,19 @@ import 'package:cloud_firestore/cloud_firestore.dart';
 Future<ConsentUser> checkForUser() async {
   final _seychellesFirestore =  Firestore.instance;
   final _auth =FirebaseAuth.instance;
-  var userName =  'Some name';
+  var userName ;
   final user = await _auth.currentUser();
   if(user != null) {
  await _seychellesFirestore.collection("Users")
         .document(user.uid). get()
         .then((value){
           userName = value.data['Name'];
-//          print(userName);
     });
-   print(userName);
-    //TODO: Fetch user's name from firestore based on the email. Make Consent User object (existingUser) based on fetched data
-    //Capitalize name
 
-//    userName = userName.substring(0, 1).toUpperCase() +
-//        userName.substring(1, userName.length);
-    //--Capitalize name
+ //--Capitalize name
+       userName= userName[0].toUpperCase() + userName.substring(1);
+
+ //--Consent User
     ConsentUser existingUser = ConsentUser(
      name: userName,
       email: user.email,
