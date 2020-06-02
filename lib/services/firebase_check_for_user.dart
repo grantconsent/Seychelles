af80a31ade37660704fe9be2107a6de9bd7a2308
Emmanuel Aliyu <53065184+Meghatronics@users.noreply.github.com>
2020-06-02 15:50:03 +0100

    Added phoneNumber from fetched document to saved data

--- lib/services/firebase_check_for_user.dart
+++ lib/services/firebase_check_for_user.dart
@@ -3,24 +3,29 @@ import 'package:grantconsent/utilities/custom_classes.dart';
 import 'package:cloud_firestore/cloud_firestore.dart';
 
 Future<ConsentUser> checkForUser() async {
-  final _seychellesFirestore =  Firestore.instance;
-  final _auth =FirebaseAuth.instance;
-  var userName ;
+  final _seychellesFirestore = Firestore.instance;
+  final _auth = FirebaseAuth.instance;
+  String userName;
+  String phoneNumber;
   final user = await _auth.currentUser();
-  if(user != null) {
- await _seychellesFirestore.collection("Users")
-        .document(user.uid). get()
-        .then((value){
-          userName = value.data['Name'];
+  if (user != null) {
+    await _seychellesFirestore
+        .collection("Users")
+        .document(user.uid)
+        .get()
+        .then((value) {
+      userName = value.data['Name'];
+      phoneNumber = value.data['PhoneNumber'];
     });
 
- //--Capitalize name
-       userName= userName[0].toUpperCase() + userName.substring(1);
+    //--Capitalize name
+    userName = userName[0].toUpperCase() + userName.substring(1);
 
- //--Consent User
+    //--Consent User
     ConsentUser existingUser = ConsentUser(
-     name: userName,
+      name: userName,
       email: user.email,
+      phoneNumber: phoneNumber,
     );
     loggedInUser = existingUser;
     return existingUser;
