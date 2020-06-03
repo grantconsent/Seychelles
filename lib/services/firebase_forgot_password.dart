0143b7238261a2d2131a3e8ae7bde431b71afdd9
Emmanuel Aliyu <53065184+Meghatronics@users.noreply.github.com>
2020-06-03 17:50:10 +0100

    Bringing back Geek's work on forgot password

--- lib/services/firebase_forgot_password.dart
+++ lib/services/firebase_forgot_password.dart
@@ -1,7 +1,24 @@
 import 'package:firebase_auth/firebase_auth.dart';
-
-Future forgotPassword(String email) async {
-  FirebaseAuth _auth = FirebaseAuth.instance;
+//Putting these changes back
+Future forgotPassword(String email)  async {
+  FirebaseAuth _auth = FirebaseAuth.instance ;
   //TODO: Try and catch exceptions. Return status based on exceptions or success.
-  return _auth.sendPasswordResetEmail(email: email);
-}
+  try  {
+    await  _auth.sendPasswordResetEmail(email: email);
+    return  ForgotStatus.success;
+  } catch  (e) {
+    print(e );
+    return e .toString().contains('ERROR_INVALID_EMAIL')
+        ?  ForgotStatus.incorrect
+        :  e.toString().contains('ERROR_USER_NOT_FOUND')
+             ? ForgotStatus.invalid
+             : ForgotStatus.internal;
+  } 
+} 
+
+ enum ForgotStatus {
+   success,
+   incorrect,
+   invalid,
+   internal,
+ }
