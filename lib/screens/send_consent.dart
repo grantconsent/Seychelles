2d5fd6e4806d04356c2aa2efd2c5abc6a2883eee
lordlamee <gabrielolamideakorede@gmail.com>
2020-06-03 01:41:10 +0100

    send consent updated

--- lib/screens/send_consent.dart
+++ lib/screens/send_consent.dart
@@ -4,8 +4,19 @@ import 'package:grantconsent/screens/dashboard.dart';
 import 'package:grantconsent/utilities/constants.dart';
 import 'package:grantconsent/utilities/styles.dart';
 import 'package:font_awesome_flutter/font_awesome_flutter.dart';
-class SendConsent extends StatelessWidget {
-  final String user = 'Celine';
+import 'package:grantconsent/services/send_through_email.dart';
+import 'package:grantconsent/services/send_through_whatsapp.dart';
+import 'package:grantconsent/services/dynamic_links_services.dart';
+
+class SendConsent extends StatefulWidget {
+  @override
+  _SendConsentState createState() => _SendConsentState();
+}
+
+class _SendConsentState extends State<SendConsent> {
+  String user = 'Celine';
+
+  String myMail;
 
   @override
   Widget build(BuildContext context) {
@@ -81,10 +92,44 @@ class SendConsent extends StatelessWidget {
                 Spacer(
                   flex: 3,
                 ),
-                UserSendButton(
-                  label: '$user\'s Email address',
-                  onPressed: () {},
-                  icon: Icons.mail_outline,
+                Builder(
+                  builder : (context)=> TextField(
+                    onChanged: (String email) {
+                      setState(() {
+                        myMail = email;
+                      });
+                      print(myMail);
+                    },
+                    onEditingComplete: () async {
+
+                      String link =
+                          await DynamicLinksService().createDynamicLinks(user);
+                      await sendConsentViaEmail(myMail, link);
+                      Scaffold.of(context).showSnackBar(SnackBar(
+                        content: Text('Message Sent'),
+                        behavior: SnackBarBehavior.floating,
+                      ));
+                     setState(() {
+                       TextEditingController().text = '';
+                     });
+                    },
+                    keyboardType: TextInputType.emailAddress,
+                    textInputAction: TextInputAction.send,
+                    autocorrect: false,
+                    textCapitalization: TextCapitalization.none,
+                    maxLines: null,
+                    decoration: InputDecoration(
+                      filled: true,
+                      fillColor: kButtonColor.withOpacity(0.3),
+                      contentPadding: EdgeInsets.all(8),
+                      labelText: '$user\'s Email',
+                      labelStyle: kBody1TextStyle.copyWith(color: Colors.grey),
+                      enabledBorder: OutlineInputBorder(
+                          borderSide: BorderSide(color: kButtonColor)),
+                      border: OutlineInputBorder(
+                          borderRadius: BorderRadius.circular(3)),
+                    ),
+                  ),
                 ),
                 Spacer(
                   flex: 2,
@@ -101,7 +146,12 @@ class SendConsent extends StatelessWidget {
                   flex: 2,
                 ),
                 UserSendButton(
-                  onPressed: () {},
+                  onPressed: () async {
+                    String link =
+                        await DynamicLinksService().createDynamicLinks(user);
+                    sendConsentViaWhatsapp(
+                        phoneNumber: '2349060232128', link: link);
+                  },
                   label: '$user\'s Whatsapp contact',
                   icon: FontAwesomeIcons.whatsapp,
                   iconColor: Colors.green,
@@ -131,6 +181,7 @@ class UserSendButton extends StatelessWidget {
   final IconData icon;
   final Function onPressed;
   final Color iconColor;
+
   @override
   Widget build(BuildContext context) {
     return Container(
