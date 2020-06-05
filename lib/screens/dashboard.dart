38a3de83d0d0c79078c67c9b7eb328c16363764e
Josteve <adekanbijosteve@gmail.com>
2020-06-05 16:34:23 +0100

    dashbaord-consent-screen

--- lib/screens/dashboard.dart
+++ lib/screens/dashboard.dart
@@ -111,31 +111,48 @@ class _DashboardState extends State<Dashboard> {
                 ),
                 SizedBox(height: 4),
                 RaisedButton(
-                  padding: EdgeInsets.zero,
+                  elevation: 0,
+                  padding: EdgeInsets.symmetric(horizontal: 20),
                   onPressed: () {},
-                  child: Text("Edit Profile"),
-                  color: Color(0xffCAB480),
+                  child: Text(
+                    "Edit Profile",
+                    style: kEditProfileTextStyle,
+                  ),
+                  color: kDashboardBackgroundColor,
+                  shape: RoundedRectangleBorder(
+                    side: BorderSide(
+                      color: Color(0xffCAB480),
+                      width: 2
+                    ),
+                    borderRadius: BorderRadius.circular(4),
+                  ),
                 ),
               ],
             ),
           )
         ],
       ),
-      decoration: BoxDecoration(color: Colors.white, boxShadow: [
-        BoxShadow(
-          color: Colors.grey,
-          blurRadius: 0.02,
-          spreadRadius: 0.02,
-          offset: Offset(0, 1.5),
-        ),
-      ]),
+      decoration: BoxDecoration(
+        color: indicatorAlignment == Alignment.centerRight
+            ? Colors.white
+            : kDashboardBackgroundColor,
+        boxShadow: indicatorAlignment == Alignment.centerRight
+            ? [
+                BoxShadow(
+                  color: Colors.grey,
+                  blurRadius: 0.02,
+                  spreadRadius: 0.02,
+                  offset: Offset(0, 1.5),
+                ),
+              ]
+            : [],
+      ),
     );
   }
 
   Widget body() {
     return Expanded(
       child: Container(
-        padding: EdgeInsets.symmetric(horizontal: 30, vertical: 25),
         color: kDashboardBackgroundColor,
         child: getBody(),
       ),
@@ -144,7 +161,13 @@ class _DashboardState extends State<Dashboard> {
 
   Widget getBody() {
     if (indicatorAlignment == Alignment.centerLeft) {
-      return Home();
+      return Padding(
+        child: Home(),
+        padding: EdgeInsets.symmetric(
+          horizontal: 30,
+          vertical: 25,
+        ),
+      );
     } else if (indicatorAlignment == Alignment.center) {
       return Consent();
     }
@@ -397,10 +420,165 @@ class Home extends StatelessWidget {
   }
 }
 
-class Consent extends StatelessWidget {
+class Consent extends StatefulWidget {
+  @override
+  _ConsentState createState() => _ConsentState();
+}
+
+class _ConsentState extends State<Consent> with SingleTickerProviderStateMixin {
+  TabController _controller;
+
+  @override
+  void initState() {
+    super.initState();
+    _controller = TabController(vsync: this, length: 3);
+    _controller.addListener(() {
+      setState(() {
+        if (_controller.index == 0) {
+          indicatorColor = Colors.white;
+        } else if (_controller.index == 1) {
+          indicatorColor = Colors.red;
+        } else if (_controller.index == 2) {
+          indicatorColor = Colors.green;
+        }
+      });
+    });
+  }
+
+  Color indicatorColor = Colors.white;
+
   @override
   Widget build(BuildContext context) {
-    return Container();
+    return Scaffold(
+      backgroundColor: Colors.transparent,
+      body: Container(
+        padding: EdgeInsets.symmetric(vertical: 25),
+        child: Column(
+          children: <Widget>[
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: Align(
+                alignment: Alignment.centerLeft,
+                child: Text(
+                  "Your\nConsent",
+                  style: kWelcomeDashboardTextStyle,
+                ),
+              ),
+            ),
+            SizedBox(height: 20),
+            Padding(
+              padding: EdgeInsets.symmetric(horizontal: 30),
+              child: TabBar(
+                controller: _controller,
+                labelStyle: kConsentTabTextStyle,
+                indicatorColor: indicatorColor,
+                tabs: <Widget>[
+                  Tab(
+                    text: "In Progress",
+                  ),
+                  Tab(text: "Rejected"),
+                  Tab(text: "Accepted"),
+                ],
+              ),
+            ),
+            SizedBox(height: 20),
+            Expanded(
+              child: TabBarView(
+                controller: _controller,
+                children: <Widget>[
+                  Container(
+                    child: ListView(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30),
+                          padding: EdgeInsets.all(12.3),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text("Bami agree to consent",
+                                  style: kDashboardOptionTextStyle),
+                              Icon(
+                                Icons.info_outline,
+                                color: Colors.black,
+                              )
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Color(0xff222222).withOpacity(0.4),
+                            border: Border.all(color: Colors.black),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: ListView(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30),
+                          padding: EdgeInsets.all(12.3),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text("Rejected Consent",
+                                  style: kDashboardOptionTextStyle),
+                              Icon(
+                                Icons.info_outline,
+                                color: Colors.red[700],
+                              )
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.red.withOpacity(0.4),
+                            border: Border.all(color: Colors.red),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                  Container(
+                    child: ListView(
+                      children: <Widget>[
+                        Container(
+                          margin: EdgeInsets.symmetric(horizontal: 30),
+                          padding: EdgeInsets.all(12.3),
+                          child: Row(
+                            mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                            children: <Widget>[
+                              Text("Accepted Consent",
+                                  style: kDashboardOptionTextStyle),
+                              Icon(
+                                Icons.info_outline,
+                                color: Colors.green[700],
+                              )
+                            ],
+                          ),
+                          decoration: BoxDecoration(
+                            color: Colors.green.withOpacity(0.4),
+                            border: Border.all(color: Colors.green),
+                            borderRadius: BorderRadius.circular(5),
+                          ),
+                        ),
+                      ],
+                    ),
+                  ),
+                ],
+              ),
+            )
+          ],
+        ),
+      ),
+      floatingActionButton: FloatingActionButton(
+        backgroundColor: Colors.white,
+        onPressed: () {},
+        child: Icon(
+          Icons.add,
+          color: Colors.black,
+        ),
+      ),
+    );
   }
 }
 
